619c3d48884375479e0a71517a0dd94d8fbfc191
manojrammurthy <manrox.drag@gmail.com>
2016-01-20 10:08:59 +0530

    added express

---
+++ Package/NPM2/array.js
@@ -0,0 +1,6 @@
+var array = require('underscore');
+array.each(
+	[1,2,3,4,5],function(num){
+		console.log('numbers:'+num);
+	}
+	);
---
+++ testfile.js
@@ -0,0 +1,10 @@
+var percent =0;
+var total=0;
+var months=0;
+
+percent=2;
+months=45;
+
+total=months*percent;
+console.log('total percent:'+total);
+console.log('todays date is '+ new Date());
